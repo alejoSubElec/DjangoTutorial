30f01917c60a4aeac05453e843cd87322637684d
Alejo <alejo@subastaelectronica.com.ar>
2022-09-05 13:59:55 -0300

    Decimo y ultimo commit de 1dia

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("5", views.detail, name="detail"),
+    path("5", views.results, name="results"),
+    path("5", views.vote, name="vote")
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 
