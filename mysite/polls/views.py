69718f2fcc9abc88178a874d282d3af80ee1048d
Alejo <alejo@subastaelectronica.com.ar>
2022-09-06 09:45:53 -0300

    templates agregados

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,5 @@
+from re import template
+from unittest import loader
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -5,8 +7,11 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ",".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.rget_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context,request))
 
 def detail(request, question_id):
     return HttpResponse("You´re looking at question %s." % question_id)
