fb59003ca989745e9018c43b2c5a61ada90ce319
Alejo <alejo@subastaelectronica.com.ar>
2022-09-06 08:50:22 -0300

    Segundo commit del dia

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("34", views.detail, name="detail"),
+    path("34:question_id", views.detail, name="detail"),
     path("34", views.results, name="results"),
     path("34", views.vote, name="vote")
 ]
