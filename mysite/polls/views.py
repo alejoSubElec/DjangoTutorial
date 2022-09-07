74b2b7b8586c79ed7b913b971607d9e807755475
Alejo <alejo@subastaelectronica.com.ar>
2022-09-07 11:51:51 -0300

    Primer commit de 5ta parte

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -18,25 +18,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
-# def index(request):
-#      latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#      template = loader.get_template("polls/index.html")
-#      context = {
-#          "latest_question_list": latest_question_list,
-#      }
-#      return render(request, "polls/index.html", context)
 
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk = question_id)
-#     return render(request, "polls/detail.html", {"question": question})
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
     
-# def results(request,question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
