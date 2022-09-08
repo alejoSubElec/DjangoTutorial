f785a8270120414f0be528017d52ada13579c184
Alejo <alejo@subastaelectronica.com.ar>
2022-09-08 12:15:54 -0300

    Serializers y views commit

--- mysite/apidjrst/quickstart/views.py
+++ mysite/apidjrst/quickstart/views.py
@@ -1,3 +1,18 @@
+import imp
 from django.shortcuts import render
+from django.contrib.auth.models import Question, Choice
+from rest_framework import viewsets
+from rest_framework import permissions
+from apidjrst.quickstart.serializers import QuestionSerializer, QuestionSerializer2, ChoiceSerializer, ChoiceSerializer2
 
-# Create your views here.
+class QuestionViewSet(viewsets.ModelViewSet):
+    "API endpoint"
+    queryset = Question.objects.all().order_by("-date_joined")
+    serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ChoiceViewSet(viewsets.ModelViewSet):
+    "API endpoint"
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+    permission_classes = [permissions.IsAuthenticated]
