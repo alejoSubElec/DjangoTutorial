46a6466fc8f6ea379e74a0088e22c4857806b5b8
Alejo <alejo@subastaelectronica.com.ar>
2022-09-08 13:53:18 -0300

    API de tutorial finalizada

--- mysite/quickstart/serializers.py
+++ mysite/quickstart/serializers.py
@@ -4,19 +4,19 @@ from rest_framework import serializers
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Question
-        fields = ["__all__"]
+        fields = "__all__"
 
 class QuestionSerializer2(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = ["__all__"]
+        fields = "__all__"
 
 class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Choice
-        fields = ["__all__"]
+        fields = "__all__"
 
 class ChoiceSerializer2(serializers.ModelSerializer):
     class Meta:
         model = Choice
-        fields = ["__all__"]
+        fields = "__all__"
