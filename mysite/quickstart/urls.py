262978903bccf6d10d8b6e730cda753958e2ce97
Alejo <alejo@subastaelectronica.com.ar>
2022-09-08 12:59:33 -0300

    API presuntamente terminada, next step: testing our API

---
+++ mysite/quickstart/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework import routers
+from mysite.quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r"questions", views.QuestionViewSet)
+router.register(r"choices", views.ChoiceViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("api-auth", include("rest_framework.urls", namespace="rest_framework"))
+]
+
