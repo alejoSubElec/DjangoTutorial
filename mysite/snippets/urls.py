d6f3428e826a0ec07541e77bbd3788751203dbcf
Alejo <alejo@subastaelectronica.com.ar>
2022-09-09 11:08:53 -0300

    Parte 1(serialization) terminada

--- mysite/snippets/urls.py
+++ mysite/snippets/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path 
+from django.urls import path, include 
 from snippets import views
 
 urlpatterns = [
-    path("snippets/", views.snippet_list),
-    path("snippets/<int:pk>/", views.snippet_detail),
+    path("", views.snippet_list),
+    path("<int:pk>/", views.snippet_detail),
 ]
