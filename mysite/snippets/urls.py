ea2796179a3852d46ababc6f6cdabe0ef5606311
Alejo <alejo@subastaelectronica.com.ar>
2022-09-12 12:35:48 -0300

    Commit de cuarta parte, sigue en error

--- mysite/snippets/urls.py
+++ mysite/snippets/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path("<int:pk>/", views.SnippetList.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+   
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+     path("api-auth/", include("rest_framework.urls")),
+]
--- mysite/snippets/views.py
+++ mysite/snippets/views.py
@@ -4,16 +4,20 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import mixins
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import permissions
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
